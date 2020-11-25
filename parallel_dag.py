1b504c081e95a7dd3fb0aceca00028a8393e5096
HP <neo2007>
2020-11-25 14:42:14 +0530

    added dag

--- parallel_dag.py
+++ parallel_dag.py
@@ -11,7 +11,7 @@ default_args = {
 
 def process(p1):
 	print(p1)
-	return 'done'
+	return 'done2'
 
 with DAG(dag_id='parallel_dag', schedule_interval='0 0 * * *', default_args=default_args, catchup=False) as dag:
 
